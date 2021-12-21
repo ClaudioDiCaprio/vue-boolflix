8aeae278c8fb63d5d1abbb26058cf49b39e51853
Claudio <claudiodicaprio89@gmail.com>
2021-12-21 16:07:56 +0100

    ProgramGuide

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <div id="app">
    <Header @movieSearch="movieResult" @tvSearch="tvResult"/>
+   <MainCont/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import ProgramGuide from './components/ProgramGuide.vue'
 export default {
   name: 'App',
   components: {
     Header,
+    ProgramGuide
+  },
+  data() {
+    return {
+      movieList: [],
+      tvList: []
+    }
+  },
+  methods: {
+    movieResult(movieArray) {
+      this.movieList = movieArray;
+    },
+    tvResult(tvArray) {
+      this.tvList = tvArray;
+    }
   }
 }
 </script>
---
+++ src/components/ProgramGuide.vue
@@ -0,0 +1,40 @@
+<template>
+    <main>
+        <Movie v-for="movie, i in movieResult" :key="'movie' + i" :movie="movie"/>
+        <TvShow v-for="tvshow, i in tvShowResult" :key="'tvshow' + i" :tvshow="tvshow"/>
+    </main>
+</template>
+
+<script>
+import Movie from '@/components/Movie.vue'
+import TvShow from '@/components/TvShow.vue'
+export default {
+    name: 'ProgramGuide',
+    components: {
+        Movie,
+        TvShow
+    },
+    props: {
+        movieRes: Array,
+        tvRes: Array
+    },
+    computed: {
+        movieResult() {
+            return this.movieRes
+        },
+        tvShowResult() {
+            return this.tvRes
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    main {
+        width: 90%;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        min-height: calc(100vh - 100px);
+    }
